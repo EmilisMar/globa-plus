a56e04c8b6335db58651c99fb597f2920e7c79a7
urba777 <deividelis7@gmail.com>
2024-11-08 15:52:54 +0200

    Fix: DataRange added to reports. DRY Code with dates has been edited to conform to the DRY principle

--- client_web/vite.config.ts
+++ client_web/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']] } })],
+	optimizeDeps: {
+		include: ['@react-google-maps/api'],
+	},
+	resolve: {
+		// Ensures that the plugin dependencies are correctly resolved
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+	server: {
+		watch: {
+		  usePolling: true, // Use polling for file changes
+		},
+	  },
 })
